f37a28533e36db1f774ccb746add7226c2391dab
yingcanxia <3042004295@qq.com>
2019-05-24 21:44:14 +0800

    添加拦截器（过滤器）

---
+++ archetype-web/src/main/java/cn/shadow/archtype/filter/LoginFilter.java
@@ -0,0 +1,51 @@
+package cn.shadow.archtype.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest req=(HttpServletRequest) request;
+		String url=req.getRequestURI();
+		System.out.println("你好我是一个快乐的过滤器,我们正在访问："+url);
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
